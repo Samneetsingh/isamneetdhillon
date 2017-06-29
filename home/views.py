b5d9f31814d301526aada5eebea23c362478501d
Samneet Singh <samneetdhillon@hotmail.com>
2017-06-28 20:48:35 -0400

    Added static files

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
